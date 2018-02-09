d3cb33d2c3d04fe67b7c50dfc4b6f5c6481a49df
kk87lk <786279458@qq.com>
2018-02-09 12:13:50 +0800

    3

--- test.c
+++ test.c
@@ -1,7 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<math.h>
+
+long double Sigma(float x)
+{
+    int i = 1;
+    long double sum = 0;
+    for (; i <= 100; i++)
+    {
+        sum = sum + (pow(x, i) / i);
+    }
+    return sum;
+}
 int main()
 {
-	
+    clock_t a, b;
+    float d;
+    long double e;
+    scanf("%f", &d);
+    a = clock();
+    e = 1 + Sigma(d);
+    printf("%Lf", e);
+    b = clock();
+    printf("\n");
+    printf("%f", (((float)(b - a))/CLK_TCK));
 }
