d7b9b5a36e8fd1aa32be0bb31e2c6898289b6f5a
kk87lk <786279458@qq.com>
2018-02-23 21:01:42 +0800

    recurbinary

--- 二分法.c
+++ 二分法.c
@@ -2,20 +2,22 @@
 #include <string.h>
 #define halflen ((sizeof(c)/sizeof(c[0]))/2)
 char c[100000];
-int recurbinary(int len,int i)
+int recurbinary(int array[],int len,int key)
 {
-    if (len == 0 )
+    int low = 0;
+    int high = len - 1;
+    int mid;
+    while (low <= high)
     {
-        return -1;
-    }
+        mid = (low + high) / 2;
 
-    len = len / 2;
-    if (c[len] == i)
-        return (len);
-    else if (c[len] > i)
-        recurbinary(len / 2, i);
-    else if (c[len] < i)
-        recurbinary(halflen + (len / 2), i);
+        if (array[mid] == key)
+            return mid;
+        if (array[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
 }
 int main()
 {
@@ -27,6 +29,6 @@ int main()
     len = sizeof(c)/sizeof(c[0]);
     int b,d;
     scanf("%d", &b);
-    d = recurbinary(len, b);
+    d = recurbinary(c[], len, b);
     printf("%d", d);
 }
